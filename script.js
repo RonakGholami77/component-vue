02835288881da91df61b344b416b745111200b84
Rgh <gholamironak88@yahoo.com>
2024-12-08 10:07:15 +0330

    Define component card

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+Vue.component("card", {
+  props: ["title", "image"],
+  data() {
+    return {};
+  },
+  template: `<div class="card">
+        <img :src="image" :alt="title" />
+        <h1>{{title}}</h1>
+      </div>`,
+});
+
 new Vue({
   el: "#app",
   data: {},
